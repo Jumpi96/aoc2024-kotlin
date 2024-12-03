2407394c83a8619d8d838b5955371fe0ecd5c32a
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2024-12-03 16:36:28 +0100

    some details

--- src/Day03.kt
+++ src/Day03.kt
@@ -8,53 +8,63 @@ fun main() {
     println("Total result: $accumulator")
 }
 
+// O(1) time complexity solution.
+// Potential improvements: using a State Machine
 fun runMultiplicationProgram(input: String, prefix: String = "mul(", separator: Char = ',',
         suffix: String = ")", enabler: String = "do()", disabler: String = "don't()"): Int {
     var enabled = true
     var i = 0
-    var accumulator = 0
+    var totalSum = 0
     var textAccumulator = ""
     while (i < input.length) {
         textAccumulator += input[i]
-        if (enabler == textAccumulator) {
-            enabled = true
-            textAccumulator = ""
-        } else if (disabler == textAccumulator) {
-            enabled = false
-            textAccumulator = ""
-        } else if (prefix == textAccumulator) {
-            var j = i+1
-            var firstArgAccum = ""
-            while (input[j].isDigit()) {
-                firstArgAccum += input.get(j)
-                j++
-            }
-            if (firstArgAccum.toIntOrNull() == null || input[j] != separator) {
+        when (textAccumulator) {
+            enabler -> {
+                enabled = true
                 textAccumulator = ""
-                continue
             }
-            j++
-
-            var secondArgAccum = ""
-            while (input[j].isDigit()) {
-                secondArgAccum += input.get(j)
-                j++
-            }
-            if (secondArgAccum.toIntOrNull() == null || input.slice(j..<j+suffix.length) != suffix) {
+            disabler -> {
+                enabled = false
                 textAccumulator = ""
-                continue
             }
-            i = j + suffix.length - 1
+            prefix -> {
+                var j = i + 1
+                var firstArgAccum = ""
+                while (input[j].isDigit()) {
+                    firstArgAccum += input.get(j)
+                    j++
+                }
+                if (firstArgAccum.toIntOrNull() == null || input[j] != separator) {
+                    textAccumulator = ""
+                    continue
+                }
+                j++
 
-            if (enabled) {
-                accumulator += firstArgAccum.toInt() * secondArgAccum.toInt()
+                var secondArgAccum = ""
+                while (input[j].isDigit()) {
+                    secondArgAccum += input.get(j)
+                    j++
+                }
+                if (secondArgAccum.toIntOrNull() == null || input.slice(j..<j + suffix.length) != suffix) {
+                    textAccumulator = ""
+                    continue
+                }
+                i = j + suffix.length - 1
+
+                if (enabled) {
+                    totalSum += firstArgAccum.toInt() * secondArgAccum.toInt()
+                }
+                textAccumulator = ""
             }
-            textAccumulator = ""
-        } else if (!prefix.startsWith(textAccumulator) && !enabler.startsWith(textAccumulator)
-                && !disabler.startsWith(textAccumulator)) {
-            textAccumulator = ""
+            else -> {
+                if (!prefix.startsWith(textAccumulator) &&
+                    !enabler.startsWith(textAccumulator) &&
+                    !disabler.startsWith(textAccumulator)) {
+                    textAccumulator = ""
+                }
+            }
         }
         i++
     }
-    return accumulator
+    return totalSum
 }
