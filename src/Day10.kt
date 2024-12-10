648f6567421c94cfc1ff454dc4ab379b3f892238
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2024-12-10 08:41:28 +0100

    for day 10

---
+++ src/Day10.kt
@@ -0,0 +1,78 @@
+import java.io.File
+
+fun main() {
+    val matrix = File("src/resources/day10/input.txt")
+        .readLines().map { it -> it.toCharArray().map { it.toString().toInt() }.toTypedArray() }.toTypedArray()
+
+    var startTime = System.currentTimeMillis()
+    println("Trailheads score: ${findTrailheads(matrix, true)}")
+    var executionTime = System.currentTimeMillis() - startTime
+    println("\nExecution time: ${executionTime}ms (${executionTime/1000.0} seconds)")
+
+    startTime = System.currentTimeMillis()
+    println("Trailheads rating score: ${findTrailheads(matrix, false)}")
+    executionTime = System.currentTimeMillis() - startTime
+    println("\nExecution time: ${executionTime}ms (${executionTime/1000.0} seconds)")
+}
+
+data class Coordinate(val row: Int, val column: Int)
+class TopographicMap(val graph: Map<Coordinate, List<Coordinate>>, val startingCoordinates: List<Coordinate>) {
+    companion object {
+        fun availableDirections(): List<Coordinate> {
+            return listOf(Coordinate(0, -1), Coordinate(1, 0), Coordinate(0, 1), Coordinate(-1, 0))
+        }
+        const val MIN_HEIGHT: Int = 0
+        const val MAX_HEIGHT: Int = 9
+    }
+}
+
+fun findTrailheads(matrix: Array<Array<Int>>, useVisited: Boolean): Int {
+    val topographicMap = matrixToTopographicMap(matrix)
+    var accumulator = 0
+
+    for (startingCoordinate in topographicMap.startingCoordinates) {
+        accumulator += dfs(matrix, topographicMap.graph, startingCoordinate, useVisited)
+    }
+    return accumulator
+}
+
+fun dfs(matrix: Array<Array<Int>>, graph: Map<Coordinate, List<Coordinate>>, coordinate: Coordinate,
+        useVisited: Boolean, visited: MutableList<Coordinate> = mutableListOf()): Int {
+    var counter = 0
+    if (useVisited) {
+        visited.add(coordinate)
+    }
+
+    if (matrix[coordinate.row][coordinate.column] == TopographicMap.MAX_HEIGHT) {
+        counter++
+    }
+
+    for (adjacent in graph.getOrDefault(coordinate, listOf())) {
+        if (!visited.contains(adjacent)) {
+            counter += dfs(matrix, graph, adjacent, useVisited, visited)
+        }
+    }
+    return counter
+}
+
+fun matrixToTopographicMap(matrix: Array<Array<Int>>): TopographicMap {
+    val graph = mutableMapOf<Coordinate, MutableList<Coordinate>>()
+    val startingCoordinates = mutableListOf<Coordinate>()
+    for (row in matrix.indices) {
+        for (column in matrix[row].indices) {
+            val coordinate = Coordinate(row, column)
+            graph.putIfAbsent(coordinate, mutableListOf())
+            for (direction in TopographicMap.availableDirections()) {
+                if (row+direction.row >= 0 && row+direction.row < matrix.size
+                    && column+direction.column >= 0 && column+direction.column < matrix[row].size
+                    && matrix[row+direction.row][column+direction.column]-1 == matrix[row][column]) {
+                    graph[coordinate]?.add(Coordinate(row+direction.row, column+direction.column))
+                }
+            }
+            if (matrix[coordinate.row][coordinate.column] == TopographicMap.MIN_HEIGHT) {
+                startingCoordinates.add(coordinate)
+            }
+        }
+    }
+    return TopographicMap(graph, startingCoordinates)
+}
