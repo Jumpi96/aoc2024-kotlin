287e583db5a5c725b358dc7ea27027be2c84745d
Juan Pablo Lorenzo <juan.lorenzo@booking.com>
2024-12-14 22:51:35 +0100

    day 9 done

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,119 +1,92 @@
 import java.io.File
+import java.util.PriorityQueue
 
 
 fun main() {
-    val input = File("src/resources/day09/simple_input.txt")
-        .readLines().first().toList().map {x -> x.toString().toInt() }.toMutableList()
+    val input = File("src/resources/day09/input.txt")
+        .readLines().first()
+    val diskMap = processDiskMap(input)
 
     var startTime = System.currentTimeMillis()
-    println("Checksum: ${defrag(input)}")
+    println("Checksum: ${defrag(diskMap)}")
     var executionTime = System.currentTimeMillis() - startTime
     println("\nExecution time: ${executionTime}ms (${executionTime/1000.0} seconds)")
 
     startTime = System.currentTimeMillis()
-    println("Checksum whole files: ${defragWholeFiles(input)}")
+    println("Checksum whole files: ${defragWholeFiles(processDiskMapToStoredFiles(diskMap))}")
     executionTime = System.currentTimeMillis() - startTime
     println("\nExecution time: ${executionTime}ms (${executionTime/1000.0} seconds)")
 }
 
-class StoredFile(val id: Int, var size: Int, var freeSpaceAfter: Int)
-
-fun defragWholeFiles(diskMap: MutableList<Int>): Long {
-    val files = processDiskMap(diskMap)
-    // 6376613827167 too low
-    // 9724294138806 too high
-    // this doesn't work fully: 14113
-
-    var fileIndex = files.size-1
-    while (fileIndex >= 0) {
-        val file = files[fileIndex]
-        var posteriorFileIndex = 0
-        while (posteriorFileIndex < fileIndex) {
-            val posteriorFile = files[posteriorFileIndex]
-
-            if (file.size <= posteriorFile.freeSpaceAfter) {
-                val sizeDiff = posteriorFile.freeSpaceAfter - file.size
-                files[fileIndex - 1].freeSpaceAfter += file.size + file.freeSpaceAfter
-                files.add(posteriorFileIndex + 1, StoredFile(file.id, file.size, sizeDiff))
-                posteriorFile.freeSpaceAfter = 0
-                files.removeAt(fileIndex + 1)
-                fileIndex++
-                break
-            }
-            posteriorFileIndex++
+class StoredFile(val start: Int, var size: Int, var fileId: Long?) {
+    fun checksum(index: Int = start) =
+        (0 ..< size).sumOf {
+            (index + it) * (fileId ?: 0L)
         }
-        fileIndex--
-    }
-    return calculateChecksum(files, true)
 }
 
-fun defrag(diskMap: MutableList<Int>): Long {
-    val files = processDiskMap(diskMap)
+// A bit of help from: https://todd.ginsberg.com/post/advent-of-code/2024/day9/
+fun defragWholeFiles(files: List<StoredFile>): Long {
+    val freeSpace = files
+        .filter { it.fileId == null }
+        .groupBy({ it.size }, {it.start})
+        .mapValues({ (_, v) -> PriorityQueue(v)})
+        .toMutableMap()
 
-    var minimumNonFilledIndex = 0
-
-    var fileIndex = files.size-1
-    while (fileIndex >= 0) {
-        val file = files[fileIndex]
-        var posteriorFileIndex = minimumNonFilledIndex
-        while (posteriorFileIndex < fileIndex) {
-            val posteriorFile = files[posteriorFileIndex]
-            if (file.size > 0) {
-                var count = 0
-                while (file.size > 0 && posteriorFile.freeSpaceAfter > 0) {
-                    posteriorFile.freeSpaceAfter--
-                    file.size--
-                    count++
-                }
-                if (count > 0) {
-                    if (posteriorFile.freeSpaceAfter == 0) {
-                        files.add(posteriorFileIndex+1, StoredFile(file.id, count, 0))
-                        minimumNonFilledIndex = posteriorFileIndex+2
-                    } else {
-                        files.add(posteriorFileIndex+1, StoredFile(file.id, count, posteriorFile.freeSpaceAfter))
-                        posteriorFile.freeSpaceAfter = 0
-                        minimumNonFilledIndex = posteriorFileIndex+1
-                    }
-                    fileIndex++
-                }
-            } else {
-                files.removeAt(fileIndex)
-                break
-            }
-            posteriorFileIndex++
-        }
-        fileIndex--
+    return files.filterNot { it.fileId == null }.reversed().sumOf { storedFile ->
+        storedFile.checksum(freeSpace.findSpace(storedFile))
     }
-    return calculateChecksum(files, false)
 }
 
-fun calculateChecksum(files: List<StoredFile>, printFiles: Boolean): Long {
-    var accumulator = 0L
-    var index = 0
-    for (file in files) {
-        for (count in 0..< file.size) {
-            accumulator += index * file.id
-            if (printFiles) {
-                print("[${file.id}]")
-            }
-            index++
+private fun MutableMap<Int, PriorityQueue<Int>>.findSpace(storedFile: StoredFile): Int =
+    (storedFile.size .. 9).mapNotNull { trySize ->
+        if (this[trySize]?.isNotEmpty() == true) trySize to this.getValue(trySize).first()
+        else null
+    }.sortedBy { it.second }.filter { it.second < storedFile.start }.firstNotNullOfOrNull { (size, startAt) ->
+        this[size]?.poll()
+        if (size != storedFile.size) {
+            val diff = size - storedFile.size
+            computeIfAbsent(diff) { _ -> PriorityQueue() }.add(startAt + storedFile.size)
         }
-        index += file.freeSpaceAfter
-        if (printFiles) {
-            for (i in 1..file.freeSpaceAfter) {
-                print('.')
-            }
-        }
+        startAt
+    } ?: storedFile.start
 
+fun defrag(diskMap: List<Long?>): Long {
+    val emptyBlocks = diskMap.indices.filter { diskMap[it] == null }.toMutableList()
+    return diskMap.withIndex().reversed().sumOf { (index, value) ->
+        if (value != null) {
+            value * (emptyBlocks.removeFirstOrNull() ?: index)
+        } else {
+            emptyBlocks.removeLastOrNull()
+            0
+        }
     }
-    println()
-    return accumulator
 }
 
-fun processDiskMap(diskMap: List<Int>): MutableList<StoredFile> {
-    val files = mutableListOf<StoredFile>()
-    for (id in diskMap.indices step 2) {
-        files.add(StoredFile(id/2, diskMap[id], if (id+1 < diskMap.size) diskMap[id+1] else 0))
+fun processDiskMap(input: String): List<Long?> =
+    input
+        .trim()
+        .windowed(2, 2, true)
+        .withIndex()
+        .flatMap { (index, value) ->
+            List(value.first().digitToInt()) { _ -> index.toLong() } +
+                    List(value.getOrElse(1){ _ -> '0' }.digitToInt()) { null }
+        }
+
+fun processDiskMapToStoredFiles(diskMap: List<Long?>): List<StoredFile> = buildList {
+    var blockStart = -1
+    var previousValue: Long? = -1L
+    diskMap.withIndex().forEach { (index, value) ->
+        if (previousValue == -1L) {
+            blockStart = index
+            previousValue = value
+        } else if (previousValue != value) {
+            add(StoredFile(blockStart, index - blockStart, previousValue))
+            blockStart = index
+            previousValue = value
+        }
     }
-    return files
+    if (blockStart != -1) {
+        add(StoredFile(blockStart, diskMap.size - blockStart, previousValue))
+    }
 }
